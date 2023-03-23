eae9b8d07e8b2e0a5e4900d1bc15339945e2f693
Kyle Holzinger <kyle@relaypayments.com>
2023-03-23 12:19:46 -0400

    bug example between the dev and build/preview scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import PhoneInputModule from "react-phone-input-2";
 import './App.css'
+
+console.log(PhoneInputModule)
 
 function App() {
   const [count, setCount] = useState(0)
